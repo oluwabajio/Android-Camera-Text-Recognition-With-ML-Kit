81e1708ea367f6f3168e9e342172ed50498f6f7e
ismail osunlana <ismailosunlana@yahoo.com>
2020-09-04 22:21:58 +0100

    First Commit

---
+++ app/src/main/java/text/recognition/ml/kit/MainActivity.java
@@ -0,0 +1,14 @@
+package text.recognition.ml.kit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
